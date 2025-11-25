43b15c8ba78d7265306f48225b77e9fe72fbea00
Hemanth1310 <hemanth13k@gmail.com>
2025-11-25 17:47:30 +0100

    backend-setup

---
+++ backend/src/data.ts
@@ -0,0 +1,24 @@
+const students = {
+        StudentData:[
+        {   
+            id:1,
+            name:'john',
+            class:'a'
+        },
+        {   
+            id:2,
+            name:'patrick',
+            class:'b'
+        },
+        {   
+            id:3,
+            name:'steve',
+            class:'a'
+        },
+        {   
+            id:4,
+            name:'truck',
+            class:'b'
+        },
+    ]}
+export default students
